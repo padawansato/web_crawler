cf23613f6c08f6017c59a5ced18fa7cd61f6be83
psato <forhighlow@gmail.com>
2017-06-05 01:45:11 +0900

    reset scrapy project

--- my_pyjob/my_pyjob/items.py
+++ my_pyjob/my_pyjob/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class PyjobItem(scrapy.Item):
+class MyPyjobItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
--- my_pyjob/my_pyjob/pipelines.py
+++ my_pyjob/my_pyjob/pipelines.py
@@ -6,6 +6,6 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class PyjobPipeline(object):
+class MyPyjobPipeline(object):
     def process_item(self, item, spider):
         return item
